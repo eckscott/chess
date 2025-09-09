61c4456531bcd00ea6a95c1c9c71ca5e740b35af
eckscott <escottie2023@gmail.com>
2025-09-09 10:41:15 -0600

    KnightMoveTests -- PASSED

--- shared/src/main/java/chess/KnightMovesCalc.java
+++ shared/src/main/java/chess/KnightMovesCalc.java
@@ -10,8 +10,7 @@ public class KnightMovesCalc implements PieceMovesCalc {
         ChessPiece currPiece = board.getPiece(myPosition);
 
         // shallow left up
-        if ((myPosition.getColumn() > MIN_COL + 2) && (myPosition.getRow() < MAX_ROW - 1)){
-            System.out.printf("Shallow left up -- entered\n");
+        if ((myPosition.getColumn() > MIN_COL + 1) && (myPosition.getRow() < MAX_ROW)){
             ChessPosition newPosition = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() - 2);
             if (board.getPiece(newPosition) == null)
                 moves.add(new ChessMove(myPosition, newPosition, null));
@@ -23,8 +22,7 @@ public class KnightMovesCalc implements PieceMovesCalc {
             }
         }
         // tall left up
-        if ((myPosition.getColumn() > MIN_COL + 1) && (myPosition.getRow() < MAX_ROW - 2)){
-            System.out.printf("Tall left up -- entered\n");
+        if ((myPosition.getColumn() > MIN_COL) && (myPosition.getRow() < MAX_ROW - 1)){
             ChessPosition newPosition = new ChessPosition(myPosition.getRow() + 2, myPosition.getColumn() - 1);
             if (board.getPiece(newPosition) == null)
                 moves.add(new ChessMove(myPosition, newPosition, null));
@@ -36,8 +34,7 @@ public class KnightMovesCalc implements PieceMovesCalc {
             }
         }
         // shallow right up
-        if ((myPosition.getColumn() < MAX_COL - 2) && (myPosition.getRow() < MAX_ROW - 1)){
-            System.out.printf("Shallow right up -- entered\n");
+        if ((myPosition.getColumn() < MAX_COL - 1) && (myPosition.getRow() < MAX_ROW)){
             ChessPosition newPosition = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() + 2);
             if (board.getPiece(newPosition) == null)
                 moves.add(new ChessMove(myPosition, newPosition, null));
@@ -49,8 +46,7 @@ public class KnightMovesCalc implements PieceMovesCalc {
             }
         }
         // tall right up
-        if ((myPosition.getColumn() < MAX_COL - 1) && (myPosition.getRow() < MAX_ROW - 2)){
-            System.out.printf("Tall right up -- entered\n");
+        if ((myPosition.getColumn() < MAX_COL) && (myPosition.getRow() < MAX_ROW - 1)){
             ChessPosition newPosition = new ChessPosition(myPosition.getRow() + 2, myPosition.getColumn() + 1);
             if (board.getPiece(newPosition) == null)
                 moves.add(new ChessMove(myPosition, newPosition, null));
@@ -63,8 +59,7 @@ public class KnightMovesCalc implements PieceMovesCalc {
         }
 
         // shallow left down
-        if ((myPosition.getColumn() > MIN_COL + 2) && (myPosition.getRow() > MIN_ROW + 1)){
-            System.out.printf("Shallow left down -- entered\n");
+        if ((myPosition.getColumn() > MIN_COL + 1) && (myPosition.getRow() > MIN_ROW)){
             ChessPosition newPosition = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() - 2);
             if (board.getPiece(newPosition) == null)
                 moves.add(new ChessMove(myPosition, newPosition, null));
@@ -76,9 +71,7 @@ public class KnightMovesCalc implements PieceMovesCalc {
             }
         }
         // tall left down
-        System.out.printf("Get Row returns: %s\n Get Column returns: %s\n", myPosition.getRow(), myPosition.getColumn());
-        if ((myPosition.getColumn() > MIN_COL + 1) && (myPosition.getRow() > MIN_ROW + 2)){
-            System.out.printf("Tall left down -- entered\n");
+        if ((myPosition.getColumn() > MIN_COL) && (myPosition.getRow() > MIN_ROW + 1)){           
             ChessPosition newPosition = new ChessPosition(myPosition.getRow() - 2, myPosition.getColumn() - 1);
             if (board.getPiece(newPosition) == null)
                 moves.add(new ChessMove(myPosition, newPosition, null));
@@ -90,8 +83,7 @@ public class KnightMovesCalc implements PieceMovesCalc {
             }
         }
         // shallow right down
-        if ((myPosition.getColumn() < MAX_COL - 2) && (myPosition.getRow() > MIN_ROW + 1)){
-            System.out.printf("Shallow right down -- entered\n");
+        if ((myPosition.getColumn() < MAX_COL - 1) && (myPosition.getRow() > MIN_ROW)){
             ChessPosition newPosition = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() + 2);
             if (board.getPiece(newPosition) == null)
                 moves.add(new ChessMove(myPosition, newPosition, null));
@@ -103,8 +95,7 @@ public class KnightMovesCalc implements PieceMovesCalc {
             }
         }
         // tall right down
-        if ((myPosition.getColumn() < MAX_COL - 1) && (myPosition.getRow() > MIN_ROW + 2)){
-            System.out.printf("Tall right down -- entered\n");
+        if ((myPosition.getColumn() < MAX_COL) && (myPosition.getRow() > MIN_ROW + 1)){
             ChessPosition newPosition = new ChessPosition(myPosition.getRow() - 2, myPosition.getColumn() + 1);
             if (board.getPiece(newPosition) == null)
                 moves.add(new ChessMove(myPosition, newPosition, null));
