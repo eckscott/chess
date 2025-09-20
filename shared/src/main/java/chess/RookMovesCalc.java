a5b207757fea6b4baee54b1ba6a2e2da53aad6cc
eckscott <escottie2023@gmail.com>
2025-09-20 17:17:25 -0600

    Fixed Rook moves logic to be 1 block of code

--- shared/src/main/java/chess/RookMovesCalc.java
+++ shared/src/main/java/chess/RookMovesCalc.java
@@ -9,85 +9,30 @@ public class RookMovesCalc implements PieceMovesCalc{
         Collection<ChessMove> moves = new ArrayList<>();
         ChessPiece currPiece = board.getPiece(myPosition);
 
-        // up
-        int r = myPosition.getRow();
-        int c = myPosition.getColumn();
-        while (true) { 
-            r++;
-            if (r > MAX_ROW)
-                break;
-            
-            ChessPosition newPosition = new ChessPosition(r, c);
-            if (board.getPiece(newPosition) == null)
-                moves.add(new ChessMove(myPosition, newPosition, null));
-            else{
-                ChessPiece newPositionPiece = board.getPiece(newPosition);
-                if (currPiece.getTeamColor() != newPositionPiece.getTeamColor()){
+        int[] rowDirections = {0, 1, 0, -1};
+        int[] colDirections = {1, 0, -1, 0};
+        for(int i = 0; i < rowDirections.length; i++){
+            int r = myPosition.getRow();
+            int c = myPosition.getColumn();
+            while (true) {
+                r = r + rowDirections[i];
+                c = c + colDirections[i];
+
+                if (r > MAX_ROW || r < MIN_ROW || c > MAX_COL || c < MIN_COL)
+                    break;
+
+                ChessPosition newPosition = new ChessPosition(r, c);
+                if (board.getPiece(newPosition) == null)
                     moves.add(new ChessMove(myPosition, newPosition, null));
-                    break;
+                else{
+                    ChessPiece newPositionPiece = board.getPiece(newPosition);
+                    if (currPiece.getTeamColor() != newPositionPiece.getTeamColor()){
+                        moves.add(new ChessMove(myPosition, newPosition, null));
+                        break;
+                    }
+                    else
+                        break;
                 }
-                else
-                    break;
-            }
-        }
-        // down
-        r = myPosition.getRow();
-        while (true) { 
-            r--;
-            if (r < MIN_ROW)
-                break;
-            
-            ChessPosition newPosition = new ChessPosition(r, c);
-            if (board.getPiece(newPosition) == null)
-                moves.add(new ChessMove(myPosition, newPosition, null));
-            else{
-                ChessPiece newPositionPiece = board.getPiece(newPosition);
-                if (currPiece.getTeamColor() != newPositionPiece.getTeamColor()){
-                    moves.add(new ChessMove(myPosition, newPosition, null));
-                    break;
-                }
-                else
-                    break;
-            }
-        }
-        // left
-        r = myPosition.getRow();
-        while (true) { 
-            c--;
-            if (c < MIN_COL)
-                break;
-            
-            ChessPosition newPosition = new ChessPosition(r, c);
-            if (board.getPiece(newPosition) == null)
-                moves.add(new ChessMove(myPosition, newPosition, null));
-            else{
-                ChessPiece newPositionPiece = board.getPiece(newPosition);
-                if (currPiece.getTeamColor() != newPositionPiece.getTeamColor()){
-                    moves.add(new ChessMove(myPosition, newPosition, null));
-                    break;
-                }
-                else
-                    break;
-            }
-        }
-        //right
-        c = myPosition.getColumn();
-        while (true) { 
-            c++;
-            if (c > MAX_COL)
-                break;
-            
-            ChessPosition newPosition = new ChessPosition(r, c);
-            if (board.getPiece(newPosition) == null)
-                moves.add(new ChessMove(myPosition, newPosition, null));
-            else{
-                ChessPiece newPositionPiece = board.getPiece(newPosition);
-                if (currPiece.getTeamColor() != newPositionPiece.getTeamColor()){
-                    moves.add(new ChessMove(myPosition, newPosition, null));
-                    break;
-                }
-                else
-                    break;
             }
         }
         return moves;
