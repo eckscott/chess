c62684769fab0f9fb21a14fc64183979540f2b49
eckscott <escottie2023@gmail.com>
2025-11-11 13:35:57 -0700

    Connected HTTP requests with register functionality in client -- SUCCESS -- now need to clear from my client so it doesn't keep duplicating users

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -3,7 +3,13 @@ import ui.PreLogin;
 
 public class Main {
     public static void main(String[] args) {
-        PreLogin preLogin = new PreLogin();
-        preLogin.run();
+        int port = 8080;
+        PreLogin preLogin = new PreLogin(port);
+        try{
+            preLogin.run();
+        }
+        catch (Exception e){
+            System.out.printf("Unable to start the server: %s%n", e.getMessage());
+        }
     }
 }
--- client/src/main/java/ui/PreLogin.java
+++ client/src/main/java/ui/PreLogin.java
@@ -1,10 +1,19 @@
 package ui;
 
+import server.ServerFacade;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PreLogin {
 
-    public void run() {
+    private final ServerFacade server;
+
+    public PreLogin(int port) {
+        server = new ServerFacade(port);
+    }
+
+    public void run() throws Exception {
         System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "♕ 240 Chess Client: type <help> for options");
 
         Scanner scanner = new Scanner(System.in);
@@ -26,13 +35,14 @@ public class PreLogin {
         System.out.print("\n" + EscapeSequences.RESET_TEXT_COLOR + ">>> " + EscapeSequences.SET_TEXT_COLOR_GREEN);
     }
 
-    public String eval(String input){
+    public String eval(String input) throws Exception {
         String[] tokens = input.toLowerCase().split(" ");
         String cmd = (tokens.length > 0) ? tokens[0] : "help";
+        String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
         return switch (cmd){
             case "quit" -> "quit";
             case "login" -> "Login functionality place holder";
-            case "register" -> "register place holder";
+            case "register" -> register(params);
             default -> help();
         };
     }
@@ -44,5 +54,16 @@ public class PreLogin {
                 login <USERNAME> <PASSWORD> - to play chess
                 register <USERNAME> <PASSWORD> <EMAIL> - to create an account
                 """;
+    }
+
+    private String register(String... params) throws Exception {
+        if (params.length == 3){
+            String username = params[0];
+            String password = params[1];
+            String email = params[2];
+            var auth = server.register(username, password, email);
+            return String.format("You have successfully registered and are now signed in as %s", auth.username());
+        }
+        throw new Exception("Didn't work");
     }
 }
